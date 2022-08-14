3a598f113406352ac89656632a7bcb49aaf2d52c
Kennedy Wambua <kennywambu254@gmail.com>
2022-08-14 09:02:12 +0300

    intialize project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1 import react andf reactDom library
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//2 Get a reference to the div with ID root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//3 Tell React to make control os that element
+const root = ReactDOM.createRoot(el);
+//4 Creat a component
+
+function App() {
+    return <input style={{ textDecoration: 'none', paddingTop: '200px', border: '1px solid blue' }} />
+}
+//5 Show the component on the screen
+
+root.render(<App />);
+
